826d87c9160782cb99a9372b1ba20e19e1d004ec
bashaer310 <bashaer_alhothali@outlook.sa>
2025-12-30 19:46:26 +0300

    Add Entity Resource and Policy

---
+++ app/Filament/Resources/Entities/Schemas/EntityForm.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\Entities\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class EntityForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('contact_info')
+                    ->required(),
+            ]);
+    }
+}
